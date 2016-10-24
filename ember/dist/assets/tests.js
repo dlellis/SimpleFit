ad3c5bcea1f2a2ea28f04a3d4f9f5151a67a19c0
dlellis <dlellis@unomaha.edu>
2016-10-23 23:58:08 -0500

    Made changes to directories

---
+++ ember/dist/assets/tests.js
@@ -0,0 +1,207 @@
+'use strict';
+
+define('simple-fit/tests/app.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint | app.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'app.js should pass jshint.');
+  });
+});
+define('simple-fit/tests/helpers/destroy-app', ['exports', 'ember'], function (exports, _ember) {
+  exports['default'] = destroyApp;
+
+  function destroyApp(application) {
+    _ember['default'].run(application, 'destroy');
+  }
+});
+define('simple-fit/tests/helpers/destroy-app.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint | helpers/destroy-app.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/destroy-app.js should pass jshint.');
+  });
+});
+define('simple-fit/tests/helpers/module-for-acceptance', ['exports', 'qunit', 'ember', 'simple-fit/tests/helpers/start-app', 'simple-fit/tests/helpers/destroy-app'], function (exports, _qunit, _ember, _simpleFitTestsHelpersStartApp, _simpleFitTestsHelpersDestroyApp) {
+  var Promise = _ember['default'].RSVP.Promise;
+
+  exports['default'] = function (name) {
+    var options = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
+
+    (0, _qunit.module)(name, {
+      beforeEach: function beforeEach() {
+        this.application = (0, _simpleFitTestsHelpersStartApp['default'])();
+
+        if (options.beforeEach) {
+          return options.beforeEach.apply(this, arguments);
+        }
+      },
+
+      afterEach: function afterEach() {
+        var _this = this;
+
+        var afterEach = options.afterEach && options.afterEach.apply(this, arguments);
+        return Promise.resolve(afterEach).then(function () {
+          return (0, _simpleFitTestsHelpersDestroyApp['default'])(_this.application);
+        });
+      }
+    });
+  };
+});
+define('simple-fit/tests/helpers/module-for-acceptance.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint | helpers/module-for-acceptance.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/module-for-acceptance.js should pass jshint.');
+  });
+});
+define('simple-fit/tests/helpers/resolver', ['exports', 'simple-fit/resolver', 'simple-fit/config/environment'], function (exports, _simpleFitResolver, _simpleFitConfigEnvironment) {
+
+  var resolver = _simpleFitResolver['default'].create();
+
+  resolver.namespace = {
+    modulePrefix: _simpleFitConfigEnvironment['default'].modulePrefix,
+    podModulePrefix: _simpleFitConfigEnvironment['default'].podModulePrefix
+  };
+
+  exports['default'] = resolver;
+});
+define('simple-fit/tests/helpers/resolver.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint | helpers/resolver.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/resolver.js should pass jshint.');
+  });
+});
+define('simple-fit/tests/helpers/start-app', ['exports', 'ember', 'simple-fit/app', 'simple-fit/config/environment'], function (exports, _ember, _simpleFitApp, _simpleFitConfigEnvironment) {
+  exports['default'] = startApp;
+
+  function startApp(attrs) {
+    var application = undefined;
+
+    var attributes = _ember['default'].merge({}, _simpleFitConfigEnvironment['default'].APP);
+    attributes = _ember['default'].merge(attributes, attrs); // use defaults, but you can override;
+
+    _ember['default'].run(function () {
+      application = _simpleFitApp['default'].create(attributes);
+      application.setupForTesting();
+      application.injectTestHelpers();
+    });
+
+    return application;
+  }
+});
+define('simple-fit/tests/helpers/start-app.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint | helpers/start-app.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/start-app.js should pass jshint.');
+  });
+});
+define('simple-fit/tests/resolver.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint | resolver.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'resolver.js should pass jshint.');
+  });
+});
+define('simple-fit/tests/router.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint | router.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'router.js should pass jshint.');
+  });
+});
+define('simple-fit/tests/routes/application.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint | routes/application.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'routes/application.js should pass jshint.');
+  });
+});
+define('simple-fit/tests/routes/login.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint | routes/login.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'routes/login.js should pass jshint.');
+  });
+});
+define('simple-fit/tests/test-helper', ['exports', 'simple-fit/tests/helpers/resolver', 'ember-qunit'], function (exports, _simpleFitTestsHelpersResolver, _emberQunit) {
+
+  (0, _emberQunit.setResolver)(_simpleFitTestsHelpersResolver['default']);
+});
+define('simple-fit/tests/test-helper.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint | test-helper.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'test-helper.js should pass jshint.');
+  });
+});
+define('simple-fit/tests/unit/routes/application-test', ['exports', 'ember-qunit'], function (exports, _emberQunit) {
+
+  (0, _emberQunit.moduleFor)('route:application', 'Unit | Route | application', {
+    // Specify the other units that are required for this test.
+    // needs: ['controller:foo']
+  });
+
+  (0, _emberQunit.test)('it exists', function (assert) {
+    var route = this.subject();
+    assert.ok(route);
+  });
+});
+define('simple-fit/tests/unit/routes/application-test.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint | unit/routes/application-test.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/routes/application-test.js should pass jshint.');
+  });
+});
+define('simple-fit/tests/unit/routes/login-test', ['exports', 'ember-qunit'], function (exports, _emberQunit) {
+
+  (0, _emberQunit.moduleFor)('route:login', 'Unit | Route | login', {
+    // Specify the other units that are required for this test.
+    // needs: ['controller:foo']
+  });
+
+  (0, _emberQunit.test)('it exists', function (assert) {
+    var route = this.subject();
+    assert.ok(route);
+  });
+});
+define('simple-fit/tests/unit/routes/login-test.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint | unit/routes/login-test.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/routes/login-test.js should pass jshint.');
+  });
+});
+/* jshint ignore:start */
+
+require('simple-fit/tests/test-helper');
+EmberENV.TESTS_FILE_LOADED = true;
+
+/* jshint ignore:end */
+//# sourceMappingURL=tests.map
