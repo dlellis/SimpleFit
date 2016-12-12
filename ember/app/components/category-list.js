0b32f9200da0e2057a9718cdb1a916921bc9f7d5
dlellis <dlellis@unomaha.edu>
2016-12-12 04:55:51 -0600

    progress with exercise/workouts

--- ember/app/components/category-list.js
+++ ember/app/components/category-list.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  store: Ember.inject.service(),
   expanded: [],
+  exercises: [],
+  errorhere: 'hi',
+  
   actions: {
     toggleExercise(value) {
     	this.expanded.forEach(function(element){
@@ -11,13 +15,42 @@ export default Ember.Component.extend({
       	value.toggleProperty('showExercise');
       	this.expanded.addObject(value)
 
-    }
+    },
+    
+    addExercise: function(ex){
+
+      if (this.exercises.includes(ex.get('name'))){
+        console.log('already here')
+        this.errorhere = 'is already in the workout';
+        return
+
+      }
+
+      else{
+        console.log('not here')
+        this.exercises.addObject(ex.get('name'))
+        var exercise = this.get('store').createRecord('clientexercise',{
+        name: ex.get('name'),
+        workout: this.get('model.workout'),
+      });
+      }
+    },
   }
 });
 
-// expand(item) {
-//   if (!item) {
-//     return;
+//   actions: {
+//     addclient: function() {
+//       var client = this.store.createRecord('client', {
+//         firstname: this.get('firstname'),
+//         lastname: this.get('lastname'),
+//       });
+//       client.save();
+//       console.log(this.get('firstname'));
+//     },
+//     delclient: function(client) {
+//       var del = this.store.findRecord('client', client.id, { backgroundReload: false }).then(function(post) {
+//   post.destroyRecord(); // => DELETE to /posts/2
+// });
+//       console.log(client.get('firstname'));
+//     },
 //   }
-//   item.toggleProperty('isExpanded', true);
-// }
